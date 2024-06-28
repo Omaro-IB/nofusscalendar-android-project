d1a5c2bcd44cbc7c011074d109a16f486638cfed
Omar <>
2024-06-28 10:11:00 +0400

    method for getting all dates between two dates

--- NoFussCalendar/app/src/main/java/DTUtils.kt
+++ NoFussCalendar/app/src/main/java/DTUtils.kt
@@ -1,14 +1,18 @@
+
 import kotlinx.datetime.Clock
+import kotlinx.datetime.DateTimeUnit
 import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.daysUntil
 import kotlinx.datetime.isoDayNumber
+import kotlinx.datetime.plus
 import kotlinx.datetime.toLocalDateTime
 
 class DTUtils {
     companion object {
+        // get number of days in a given month/year
         fun getMonthDays(year: Int, month: Int): Int {
             val x = LocalDate(year, month, 1)
             return when(month){
@@ -17,6 +21,7 @@ class DTUtils {
             }
         }
 
+        // get the day of week (starting on sunday) of a given day/month/year
         fun dateToWeekDay(year: Int, month: Int, day: Int): Int {
             return when(val wd = LocalDate(year, month, day).dayOfWeek.isoDayNumber){
                 7 -> 1
@@ -24,24 +29,44 @@ class DTUtils {
             }
         }
 
+        // get all dates (in a 2D array of [[Y,M,D],...]) between two given dates (inclusive)
+        fun getDaysBetween(year1: Int, month1: Int, day1: Int, year2: Int, month2: Int, day2: Int): Array<Array<Int>> {
+            val date1 = LocalDate(year1, month1, day1)
+            val date2 = LocalDate(year2, month2, day2)
+            var date = date1
+            var y: Int; var m: Int; var d: Int
+            var returnArray: Array<Array<Int>> = arrayOf()
+            while (date in (date1..date2)) {
+                y = date.year; m = date.monthNumber; d = date.dayOfMonth
+                returnArray += arrayOf(y, m, d)
+                date = date.plus(1, DateTimeUnit.DAY)
+            }
+            return returnArray
+        }
+
+        // get current LocalDateTime
         fun getNow(): LocalDateTime{
             val currentMoment: Instant = Clock.System.now()
             val datetimeInSystemZone: LocalDateTime = currentMoment.toLocalDateTime(TimeZone.currentSystemDefault())
             return datetimeInSystemZone
         }
 
+        // get current month
         fun getMonth(): Int{
             return getNow().monthNumber
         }
 
+        // get current year
         fun getYear(): Int{
             return getNow().year
         }
 
+        // get current day
         fun getDay(): Int{
             return getNow().dayOfMonth
         }
 
+        // convert month integer (1-12) to a string representation
         fun monthIntToStr(month: Int, short: Boolean = false): String{
             if (short) {
                 return when(month){
@@ -76,6 +101,7 @@ class DTUtils {
             }
         }
 
+        // convert weekday integer (1-7, starting sunday) to a string representation
         fun weekDayIntToStr(weekday: Int, short: Boolean = false): String{
             if (short) {
                 return when(weekday){
@@ -100,6 +126,7 @@ class DTUtils {
             }
         }
 
+        // convert given hour/minute integer time to a string representation
         fun timeToStr(hour: Int, minute: Int, military: Boolean = false): String {
             if (military) {
                 return "${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}"
